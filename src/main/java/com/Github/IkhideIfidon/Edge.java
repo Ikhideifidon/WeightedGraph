cc10fd01adf86ee6fcc7dfd012b40cacb554e716
Ikhideifidon <ikhideifidon@yahoo.com>
2022-10-15 12:46:20 -0400

    WeightedGraph

--- src/main/java/com/Github/IkhideIfidon/Edge.java
+++ src/main/java/com/Github/IkhideIfidon/Edge.java
@@ -23,30 +23,25 @@ public class Edge implements Comparable<Edge> {
     public double weight() { return weight; }
 
     @Override
+    // Comparison of edges is only ideally defined on the weights.
     public int compareTo(Edge that) {
         // return this.weight > that.weight ? 1 : this.weight < that.weight ? -1 : 0;
         return Double.compare(this.weight, that.weight);
     }
 
     @Override
+    // The equality of this edge is determined by the weight only
     public boolean equals(Object o) {
         if (o == null) return false;
         if (!(o instanceof Edge that)) return false;
-        // Compare start
-        boolean compareStart = this.either() == that.either();
-        if (!compareStart) return false;
-        // Compare end
-        boolean compareEnd = this.other(v) == that.other(v);
-        if (!compareEnd) return false;
         // Compare weight
         return this.weight() == that.weight();
     }
 
     @Override
+    // The hashCode is weight dependent only
     public int hashCode() {
         int result = 0;
-        result = result + 31 * Integer.hashCode(v);
-        result = result + 31 * Integer.hashCode(w);
         return result + 31 * Double.hashCode(weight);
     }
 
